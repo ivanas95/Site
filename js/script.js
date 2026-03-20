3e3f1c380d84d09bb9aa4dbf84998dc6f610685f
ivanas95 <ivanastojkov@MacBook-Air-7.local>
2026-03-20 14:48:42 -0400

    changed a lottttt

---
+++ js/script.js
@@ -0,0 +1,83 @@
+$('.toggleHide-quality').on('click', function(e) {
+
+    e.preventDefault(); // stops page jump
+    $('.hidden-weight').show();
+
+});
+
+
+$('.toggleHide-thinner').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-thinner').show();
+
+})
+$('.toggleHide-thicker').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-thicker').show();
+
+});
+$('.option').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-mm-scale').show();
+
+});
+
+$('.toggleHide-np1, .numberList, .toggleHide-mmscale').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-ink').show();
+
+});
+
+
+$('.toggleHide-ink').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-feel').show();
+
+});
+
+$('.toggleHide-feel').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-body').show();
+
+});
+
+$('.toggleHide-body').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-closure').show();
+
+});
+
+$('.toggleHide-closure').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-grip').show();
+
+});
+
+$('.toggleHide-griptype').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-griptype').show();
+
+});
+
+$('.toggleHide-grip').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-colour').show();
+
+});
+
+$('.toggleHide-colour').on('click', function(e) {
+
+    e.preventDefault(); 
+    $('.hidden-brand').show();
+
+});
